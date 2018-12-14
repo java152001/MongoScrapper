1340726b3177393bba8a1f7fa0ffb89bab9939ed
Anthony Demott <java152001@gmail.com>
2018-12-13 23:19:20 -0700

    added semi colon

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapper"
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapper";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
